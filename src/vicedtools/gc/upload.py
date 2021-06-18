bc9a4cf49627646bf00d9e7402d3c4aaea889ef2
Greg Breese <gregsurname@gmail.com>
2021-06-19 03:58:40 +1000

    improved documentation of vicedtools.gc

--- src/vicedtools/gc/upload.py
+++ src/vicedtools/gc/upload.py
@@ -1,4 +1,20 @@
+# Copyright 2021 VicEdTools authors
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Utilities for uploading data to GC."""
+
 import pandas as pd
+
 from google.cloud import storage
 from google.cloud import bigquery
 
@@ -46,13 +62,17 @@ REPORTS_SUMMARY_CLUSTERING_FIELDS = [
 ]
 
 
-def upload_blob(bucket_name, source_file_name, destination_blob_name):
+def upload_blob(bucket_name: str, source_file_name: str,
+                destination_blob_name: str) -> None:
     """Uploads a file to the gc bucket.
+
+    Copied from
+    https://github.com/GoogleCloudPlatform/python-docs-samples/blob/HEAD/storage/cloud-client/storage_upload_file.py
     
-    Arguments
-    bucket_name: name of gc storage bucket
-    source_file_name: path of local file
-    destination_blob_name: name of destination gc storage blob
+    Args:
+        bucket_name: The name of the GC storage bucket
+        source_file_name: The path of local file to upload
+        destination_blob_name: The Name of destination GC storage blob
     """
     storage_client = storage.Client()
     bucket = storage_client.bucket(bucket_name)
@@ -62,8 +82,20 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
                                            destination_blob_name))
 
 
-def update_table(schema, clustering_fields, uri, table_id):
+def update_table(schema: list, clustering_fields: list, uri: str,
+                 table_id: str) -> None:
     '''Updates a table in bigquery.
+
+    Copied from
+    https://github.com/googleapis/python-bigquery/blob/35627d145a41d57768f19d4392ef235928e00f72/samples/load_table_uri_csv.py
+
+    Args:
+        schema: A list of bigquery.SchemaField to define to schema
+        clustering_fields: A list of up to four names of fields
+        uri: The GC uri to upload the file from
+            E.g. "gs://cloud-samples-data/bigquery/us-states/us-states.csv"
+        table_id: The BigQuery table ID.
+            E.g. "your-project.your_dataset.your_table_name
     '''
     client = bigquery.Client()
 
@@ -84,13 +116,20 @@ def update_table(schema, clustering_fields, uri, table_id):
     print("Loaded {} rows.".format(destination_table.num_rows))
 
 
-def upload_student_details(source_file, table_id, bucket):
+def upload_student_details(source_file: str, table_id: str,
+                           bucket: str) -> None:
     '''Updates a students details table in bigquery.
 
-    Arguments
-    source_file: the student details csv file
-    table_id: the bigquery table_id of your student details table
-    bucket: the name of your GC storage bucket
+    Takes a student details csv file as produced from 
+        vicedtools.gc.format.create_student_details_gc_csv(...)
+    and uploads to under 
+        ./student details/student details.csv 
+    in the given GCS bucket.
+
+    Args:
+        source_file: The path of the student details csv file.
+        table_id: The bigquery table ID of the student details table.
+        bucket: The name of the GC storage bucket.
     '''
     blob_name = "student details/student details.csv"
     uri = "gs://" + bucket + "/" + blob_name
@@ -99,14 +138,19 @@ def upload_student_details(source_file, table_id, bucket):
                  table_id)
 
 
-def upload_reports(source_file, table_id, bucket):
+def upload_reports(source_file: str, table_id: str, bucket: str) -> None:
     '''Updates a reports table in bigquery.
 
-    Arguments
-    source_file: the reports csv file
-        e.g. from vicedtools.compass.Reports.saveReports(source_file)
-    table_id: the bigquery table_id of your student details table
-    bucket: the name of your GC storage bucket
+    Takes a reports csv as produced from
+        vicedtools.compass.Reports.saveReports(source_file)
+    and uploads it under
+        ./reports/reports.csv
+    in the given GCS bucket.
+
+    Args:
+        source_file: The path of the reports csv file.
+        table_id: The bigquery table ID of the student details table.
+        bucket: The name of the GC storage bucket.
     '''
     blob_name = "reports/reports.csv"
     uri = "gs://" + bucket + "/" + blob_name
@@ -114,14 +158,20 @@ def upload_reports(source_file, table_id, bucket):
     update_table(REPORTS_SCHEMA, REPORTS_CLUSTERING_FIELDS, uri, table_id)
 
 
-def upload_reports_summary(source_file, table_id, bucket):
+def upload_reports_summary(source_file: str, table_id: str,
+                           bucket: str) -> None:
     '''Updates a reports summary table in bigquery.
 
-    Arguments
-    source_file: the reports csv file
-        e.g. from vicedtools.compass.Reports.saveSummary(source_file)
-    table_id: the bigquery table_id of your student details table
-    bucket: the name of your GC storage bucket
+    Takes a reports csv as produced from
+        vicedtools.compass.Reports.saveSummary(source_file)
+    and uploads it under
+        ./reports/reports.csv
+    in the given GCS bucket.
+
+    Args:
+        source_file: The path of the reports summary csv file.
+        table_id: The bigquery table ID of the student details table.
+        bucket: The name of the GC storage bucket.
     '''
     blob_name = "reports/reports_summary.csv"
     uri = "gs://" + bucket + "/" + blob_name
