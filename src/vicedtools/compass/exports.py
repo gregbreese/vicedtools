b450ffc92f57e838c7ab38762b5a30b4a3af6ee1
Greg Breese <gregsurname@gmail.com>
2021-06-30 01:14:41 +1000

    Update exports.py

--- src/vicedtools/compass/exports.py
+++ src/vicedtools/compass/exports.py
@@ -28,7 +28,7 @@ import zipfile
 # TODO: implement Chrome support
 def getCompassDriver(school_code: str,
                      geckodriver_path: str,
-                     download_path: str,
+                     download_path: str = "./",
                      auth: str = 'cli') -> webdriver.Firefox:
     """Creates a webdriver with Compass authentication completed.
     
@@ -85,9 +85,10 @@ def getCompassDriver(school_code: str,
     else:
         raise ValueError("auth value '" + auth + "' not valid.")
 
+
 def sds_export(school_code: str,
                geckodriver_path: str,
-               download_path: str,
+               download_path: str = ".\\",
                auth='cli',
                download_wait: int = 10 * 60,
                append_date: bool = False) -> None:
@@ -119,7 +120,7 @@ def sds_export(school_code: str,
     '''
     driver = getCompassDriver(school_code,
                               geckodriver_path,
-                              download_path,
+                              download_path=download_path,
                               auth=auth)
 
     # download Microsoft SDS export
@@ -152,7 +153,10 @@ def sds_export(school_code: str,
     #     if dt > newest_time:
     #         file_to_extract = file
     file_to_extract = files[0]
-    contents = ["StudentEnrollment.csv", "Teacher.csv", "TeacherRoster.csv","Section.csv"]
+    contents = [
+        "StudentEnrollment.csv", "Teacher.csv", "TeacherRoster.csv",
+        "Section.csv"
+    ]
     with zipfile.ZipFile(file_to_extract, 'r') as zip_ref:
         for content in contents:
             if append_date:
@@ -168,9 +172,9 @@ def sds_export(school_code: str,
 
 
 def export_student_details(school_code: str,
-                           download_path: str,
-                           auth: str,
-                           geckodriver_path: str = "") -> None:
+                           geckodriver_path: str = "",
+                           download_path: str = "student details.csv",
+                           auth: str = 'cli') -> None:
     '''Exports student details from Compass.
 
     Args:
@@ -194,7 +198,7 @@ def export_student_details(school_code: str,
     elif auth == 'cli':
         driver = getCompassDriver(school_code,
                                   geckodriver_path,
-                                  download_path,
+                                  download_path=download_path,
                                   auth='cli')
         headers = {
             "User-Agent":
@@ -218,8 +222,40 @@ def export_student_details(school_code: str,
         f.write(r.content)
 
 
-# TODO: Break up downloading one learning task academic year and the discovery
-#   of what years are available/require downloading
+def discoverAcademicYears(school_code: str,
+                          geckodriver_path: str,
+                          auth='cli') -> list(str):
+    """Discovers the academic years that exist in Compass.
+    
+    Useful for downloading learning tasks.
+
+    Args:
+        compass_school_code: Your Compass school code.
+        geckodriver_path: The location of geckodriver.exe
+        auth: Either 'cli' or 'browser'. If 'cli' will request a username and
+            password from the command line. If 'browser' will use cookies from
+            the local Firefox installation.
+
+    Returns:
+        A list of the names of each academic year.
+    """
+    driver = getCompassDriver(school_code, geckodriver_path, auth=auth)
+    driver.get(
+        "https://" + school_code +
+        ".compass.education/Communicate/LearningTasksAdministration.aspx")
+    # Open Reports tab
+    button = driver.find_element_by_id("tab-1101-btnIconEl")
+    button.click()
+    # Open dropdown menu
+    button = driver.find_element_by_id("ext-gen1188")
+    button.click()
+    # Get dropdown menu items
+    items = driver.find_elements_by_class_name("x-boundlist-item")
+    academic_years = [item.text for item in items]
+    driver.quit()
+    return academic_years
+
+
 def exportLearningTasks(school_code: str,
                         geckodriver_path: str,
                         download_path: str,
@@ -233,8 +269,7 @@ def exportLearningTasks(school_code: str,
     Will prompt for Compass login details.
     Requires access to Learning Tasks Administration.
 
-    Saves a file called "LearningTasks-[academic-year].csv" in the folder
-    specified in download_path.
+    Data is saved as "[download_path]/LearningTasks-[academic_year].csv".
 
     Args:
         compass_school_code: Your Compass school code.
@@ -242,14 +277,13 @@ def exportLearningTasks(school_code: str,
         download_path: The directory to save the export. Must use \\ slashes in 
                         windows.
         academic_year: Which Compass academic year to download the export for.
-            Use 'all' to download all available years.
         auth: Either 'cli' or 'browser'. If 'cli' will request a username and
             password from the command line. If 'browser' will use cookies from
             the local Firefox installation.
     """
     driver = getCompassDriver(school_code,
                               geckodriver_path,
-                              download_path,
+                              download_path=download_path,
                               auth=auth)
 
     driver.get(
@@ -261,74 +295,80 @@ def exportLearningTasks(school_code: str,
     # Open dropdown menu
     button = driver.find_element_by_id("ext-gen1188")
     button.click()
-    # Get dropdown menu items
-    items = driver.find_elements_by_class_name("x-boundlist-item")
-    academic_years = [item.text for item in items]
-    if academic_year == all:
-        for year in academic_years:
-            # select the academic year
-            button = driver.find_element_by_xpath("//*[contains(text(),'" +
-                                                  year + "')]")
+    # select the academic year
+    button = driver.find_element_by_xpath("//*[contains(text(),'" +
+                                          academic_year + "')]")
+    button.click()
+    # Press export button
+    button = driver.find_element_by_id("button-1061-btnInnerEl")
+    button.click()
+    for _i in range(600):  # 10 minutes
+        time.sleep(1)
+        try:
+            # Get cancel button
+            button = driver.find_element_by_xpath(
+                "//*[contains(text(),'Cancel')]")
+        except NoSuchElementException:
+            button = driver.find_element_by_xpath(
+                "//*[contains(text(),'Close')]")
             button.click()
-            # Press export button
-            button = driver.find_element_by_id("button-1061-btnInnerEl")
-            button.click()
-            for _i in range(600):  # 10 minutes
-                time.sleep(1)
-                try:
-                    # Get cancel button
-                    button = driver.find_element_by_xpath(
-                        "//*[contains(text(),'Cancel')]")
-                    # Get 'Generating' banner
-                    #banner = driver.find_element_by_xpath("//*[contains(text(),'Generating')]")
-                except NoSuchElementException:
-                    button = driver.find_element_by_xpath(
-                        "//*[contains(text(),'Close')]")
-                    button.click()
-                    break
-    elif academic_year in academic_years:
-        # select the academic year
-        button = driver.find_element_by_xpath("//*[contains(text(),'" +
-                                              academic_year + "')]")
-        button.click()
-        # Press export button
-        button = driver.find_element_by_id("button-1061-btnInnerEl")
-        button.click()
-        for _i in range(600):  # 10 minutes
-            time.sleep(1)
-            try:
-                # Get cancel button
-                button = driver.find_element_by_xpath(
-                    "//*[contains(text(),'Cancel')]")
-                # Get 'Generating' banner
-                #banner = driver.find_element_by_xpath("//*[contains(text(),'Generating')]")
-            except NoSuchElementException:
-                button = driver.find_element_by_xpath(
-                    "//*[contains(text(),'Close')]")
-                button.click()
-                break
+            break
+    driver.quit()
+
+
+def discoverProgressReportCycles(school_code: str,
+                                 geckodriver_path: str,
+                                 auth: str = 'cli',
+                                 published_only: bool = True) -> list(str):
+    """Discovers the available progress report cycles.
+
+    Args:
+        compass_school_code: Your Compass school code.
+        geckodriver_path: The location of geckodriver.exe
+        auth: Either 'cli' or 'browser'. If 'cli' will request a username and
+            password from the command line. If 'browser' will use cookies from
+            the local Firefox installation.
+        published_only: if true, only return those cycles set as "Published to 
+            All".
+
+    Returns:
+        A list of the names of each progress report cyle.
+    """
+    driver = getCompassDriver(school_code, geckodriver_path, auth=auth)
+    # open progress reports page
+    driver.get("https://" + school_code +
+               ".compass.education/Organise/Reporting/GPA/Default.aspx")
+    # open cycles tab
+    button = driver.find_element_by_xpath("//*[contains(text(),'Cycles')]")
+    button.click()
+    # all published progress reports
+    if published_only:
+        elements = driver.find_elements_by_xpath(
+            "//table/tbody/tr/td/div[contains(text(),'Published To All')]/parent::*/parent::*/child::td[1]/div/a"
+        )
     else:
-        print("Academic year '" + academic_year + "' not found.")
+        elements = driver.find_elements_by_xpath(
+            "//table/tbody/tr/td/div/parent::*/parent::*/child::td[1]/div/a")
+    progress_report_cycles = [e.text for e in elements]
+    driver.quit()
+    return progress_report_cycles
 
-    driver.close()
 
-
-# TODO: Break up downloading one progress report cycle and the discovery
-#   of what cycles are available/require downloading
 def exportProgressReports(school_code: str,
                           geckodriver_path: str,
-                          download_path: str,
                           cycle: str,
+                          download_path: str,
                           auth: str = 'cli') -> None:
     """Export progress report data.
     
+    Data is saved as "[download_path]/[cycle].csv"
+
     Args:
         compass_school_code: Your Compass school code.
         geckodriver_path: The location of geckodriver.exe
         download_path: The directory to save the export. Must use \\ slashes in 
                         windows.
-        academic_year: Which Compass academic year to download the export for.
-            Use 'all' to download all available years.
+        cycle
         auth: Either 'cli' or 'browser'. If 'cli' will request a username and
             password from the command line. If 'browser' will use cookies from
             the local Firefox installation.
@@ -336,7 +376,7 @@ def exportProgressReports(school_code: str,
 
     driver = getCompassDriver(school_code,
                               geckodriver_path,
-                              download_path,
+                              download_path=download_path,
                               auth=auth)
 
     # open progress reports page
@@ -345,78 +385,67 @@ def exportProgressReports(school_code: str,
     # open cycles tab
     button = driver.find_element_by_xpath("//*[contains(text(),'Cycles')]")
     button.click()
-    # all published progress reports
-    progress_report_cycles = [
-        e.text for e in driver.find_elements_by_xpath(
-            "//table/tbody/tr/td/div[contains(text(),'Published To All')]/parent::*/parent::*/child::td[1]/div/a"
-        )
-    ]
+    # open selected cycle
+    link = driver.find_element_by_xpath("//td/div/a[contains(text(), '" +
+                                        cycle + "')]")
+    link.click()
+    # export link
+    link = driver.find_element_by_link_text("Export results to CSV")
+    link.click()
+    # Export button
+    button = driver.find_element_by_xpath(
+        "//a/span/span/span[contains(text(),'Export')]")
+    button.click()
+    # Confirmation OK button
+    button = driver.find_element_by_xpath(
+        "//a/span/span/span[contains(text(),'OK')]")
+    button.click()
 
-    if cycle == 'all':
-        for this_cycle in progress_report_cycles:
-            link = driver.find_element_by_xpath(
-                "//td/div/a[contains(text(), '" + this_cycle + "')]")
-            link.click()
-
-            # export link
-            link = driver.find_element_by_link_text("Export results to CSV")
-            link.click()
-
-            button = driver.find_element_by_xpath(
-                "//a/span/span/span[contains(text(),'Export')]")
-            button.click()
-
-            button = driver.find_element_by_xpath(
-                "//a/span/span/span[contains(text(),'OK')]")
-            button.click()
-
-            try:
-                # Get cancel button
-                button = driver.find_element_by_xpath(
-                    "//*[contains(text(),'Cancel')]")
-                # Get 'Generating' banner
-                banner = driver.find_element_by_xpath(
-                    "//*[contains(text(),'Generating')]")
-            except NoSuchElementException:
-                button = driver.find_element_by_xpath(
-                    "//*[contains(text(),'Close')]")
-                button.click()
-
-        # back to progres reports page
-        link = driver.find_element_by_link_text("Back to Progress Reports")
-        link.click()
-        # open cycles tab
-        button = driver.find_element_by_xpath("//*[contains(text(),'Cycles')]")
-        button.click()
-    elif cycle in progress_report_cycles:
-        link = driver.find_element_by_xpath("//td/div/a[contains(text(), '" +
-                                            cycle + "')]")
-        link.click()
-
-        # export link
-        link = driver.find_element_by_link_text("Export results to CSV")
-        link.click()
-
-        button = driver.find_element_by_xpath(
-            "//a/span/span/span[contains(text(),'Export')]")
-        button.click()
-
-        button = driver.find_element_by_xpath(
-            "//a/span/span/span[contains(text(),'OK')]")
-        button.click()
-
+    for _i in range(600):  # 10 minutes
+        time.sleep(1)
         try:
             # Get cancel button
             button = driver.find_element_by_xpath(
                 "//*[contains(text(),'Cancel')]")
-            # Get 'Generating' banner
-            banner = driver.find_element_by_xpath(
-                "//*[contains(text(),'Generating')]")
         except NoSuchElementException:
             button = driver.find_element_by_xpath(
                 "//*[contains(text(),'Close')]")
             button.click()
-    else:
-        print("Progress report cycle '" + cycle + "' not found.")
+            break
+    driver.quit()
 
-    driver.close()
+def discoverReportCycles(school_code: str,
+                                 geckodriver_path: str,
+                                 auth: str = 'cli',
+                                 published_only: bool = True) -> list(tuple(str)):
+    """Discovers the available report cycles.
+
+    Args:
+        compass_school_code: Your Compass school code.
+        geckodriver_path: The location of geckodriver.exe
+        auth: Either 'cli' or 'browser'. If 'cli' will request a username and
+            password from the command line. If 'browser' will use cookies from
+            the local Firefox installation.
+        published_only: if true, only return those cycles set to allow student 
+            access.
+
+    Returns:
+        A list of the names of each report cyle.
+    """
+    driver = getCompassDriver(school_code, geckodriver_path, auth=auth)
+    # open progress reports page
+    driver.get("https://" + school_code +
+               ".compass.education/Organise/Reporting/Cycles.aspx")
+
+    rows = driver.find_elements_by_xpath("//div/div[3]/div/table/tbody/tr")
+    cycles = []
+    for row in rows:
+        published_to_students = row.find_element_by_xpath("./td[5]").text
+        if not published_only or published_to_students == "Yes":
+            year = row.find_element_by_xpath("./td[1]").text
+            name = row.find_element_by_xpath("./td[2]").text
+            cycles.append( (year,name))
+
+    driver.quit()
+    return cycles
+ 
