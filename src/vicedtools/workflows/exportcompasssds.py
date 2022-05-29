5fd8981c268f7a742673ea0864b18a340c21b737
Greg Breese <gregsurname@gmail.com>
2022-05-30 09:34:49 +1000

    Update exportcompasssds.py
    Added option to force re-download.

--- src/vicedtools/workflows/exportcompasssds.py
+++ src/vicedtools/workflows/exportcompasssds.py
@@ -13,15 +13,39 @@
 # limitations under the License.
 """Executable script for exporting the SDS export from Compass."""
 
+import argparse
+import json
 import os
+import sys
 
 from vicedtools.compass import CompassSession
 
 if __name__ == "__main__":
-    from config import (sds_dir, compass_authenticator, compass_school_code)
+    from config import (sds_dir, compass_authenticator, compass_school_code, academic_groups_json)
 
-    if not os.path.exists(sds_dir):
-        os.makedirs(sds_dir)
+    parser = argparse.ArgumentParser(
+        description='Export the SDS export from Compass')
+    parser.add_argument('-academicgroup', help='the name of the academic group, for example "2022 Academic"')
+    args = parser.parse_args()
 
-    s = CompassSession(compass_school_code, compass_authenticator)
-    s.export_sds(save_dir=sds_dir)
+    if args.academicgroup:
+        with open(academic_groups_json, 'r', encoding='utf-8') as f:
+            academic_groups = json.load(f)
+        for g in academic_groups:
+            academic_group_id = None
+            if g['name'] == args.academicgroup:
+                academic_group_id = g['id']
+                break
+        if not academic_group_id:
+            print("Academic group name not found.")
+            sys.exit(3)
+        save_dir = os.path.join(sds_dir, args.academicgroup)
+        os.makedirs(save_dir)
+        s = CompassSession(compass_school_code, compass_authenticator)
+        s.export_sds(save_dir=save_dir, academic_group=academic_group_id)
+    else:
+        if not os.path.exists(sds_dir):
+            os.makedirs(sds_dir)
+
+        s = CompassSession(compass_school_code, compass_authenticator)
+        s.export_sds(save_dir=sds_dir)
