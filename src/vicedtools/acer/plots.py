2e588949bdb8934f25f9a51de1cdf45d68c19142
Greg Breese <gregsurname@gmail.com>
2022-11-21 00:40:20 +1100

    Added student time series plot

--- src/vicedtools/acer/plots.py
+++ src/vicedtools/acer/plots.py
@@ -15,6 +15,10 @@
 
 from __future__ import annotations
 
+import json
+import os
+
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
@@ -245,3 +249,247 @@ def item_analysis_plots(results: PATResultsCollection,
                                                to_date=to_date)
                     f.savefig(filename, dpi=150, facecolor="white")
                     plt.close(f)
+
+
+def time_series_background_coords(scale_constructs_json,
+                                  cmap=mpl.colormaps['PiYG']):
+    with open(scale_constructs_json, 'r') as f:
+        scale_constructs = json.load(f)
+
+    xs = [0.84, 1.84, 2.84, 3.84, 4.84, 5.84, 6.84, 7.84, 8.84, 9.84, 10]
+    background_coords = {}
+    background_coords['xs'] = xs
+
+    tests = ["Reading", "Maths"]
+    for test in tests:
+        # find scale construct for test
+        for key in scale_constructs:
+            if test in scale_constructs[key]['name']:
+                scale_construct = scale_constructs[key]
+
+        # extract percentiles for the test from the Australian norm sample
+        percentiles = ['5', '25', '75', '95']
+        norm_values = {}
+        for percentile in percentiles:
+            norm_values[percentile] = []
+            for yr in scale_construct['metadata']['normReferencedLevels']:
+                norm_values[percentile].append(
+                    yr['percentiles'][percentile]['scaleScore'])
+        # add extrapolated percentile values for the end of Year 10 (treating the Year 10 result as for October/November)
+        for percentile in ['5', '25', '75', '95']:
+            norm_values[percentile].append(norm_values[percentile][-1] +
+                                           (norm_values[percentile][-1] -
+                                            norm_values[percentile][-2]) * 0.16)
+
+        background_coords[test] = {
+            'Well above expected level': {},
+            'Above expected level': {},
+            'At expected level': {},
+            'Below expected level': {},
+            'Well below expected level': {},
+        }
+        background_coords[test]['Well above expected level']['top'] = [
+            200 for _ in range(len(xs))
+        ]
+        background_coords[test]['Well above expected level']['bottom'] = norm_values['95']
+        background_coords[test]['Above expected level']['top'] = norm_values['95']
+        background_coords[test]['Above expected level']['bottom'] = norm_values['75']
+        background_coords[test]['At expected level']['top'] = norm_values['75']
+        background_coords[test]['At expected level']['bottom'] = norm_values['25']
+        background_coords[test]['Below expected level']['top'] = norm_values['25']
+        background_coords[test]['Below expected level']['bottom'] = norm_values['5']
+        background_coords[test]['Well below expected level']['top'] = norm_values['5']
+        background_coords[test]['Well below expected level']['bottom'] = [
+            0 for _ in range(len(xs))
+        ]
+
+        background_coords[test]['Well above expected level']['colour'] = cmap(
+            200)
+        background_coords[test]['Above expected level']['colour'] = cmap(164)
+        background_coords[test]['At expected level']['colour'] = cmap(128)
+        background_coords[test]['Below expected level']['colour'] = cmap(92)
+        background_coords[test]['Well below expected level']['colour'] = cmap(
+            56)
+
+    return background_coords
+
+
+def student_time_series_plot(scores, background_coords):
+    plt.rcParams.update({
+        'font.sans-serif': 'Arial',
+        'font.family': 'sans-serif'
+    })
+
+    # Create the figure and axes
+    fig = plt.figure(figsize=(9.5, 4))
+    gs = fig.add_gridspec(3,
+                          3,
+                          width_ratios=(9, 9, 1),
+                          height_ratios=[1, 2, 1],
+                          left=0.1,
+                          right=0.9,
+                          bottom=0.1,
+                          top=0.9,
+                          wspace=0.35,
+                          hspace=0.05)
+    axs = {}
+    axs["Reading"] = fig.add_subplot(gs[:, 0])
+    axs["Maths"] = fig.add_subplot(gs[:, 1])
+    cbar = fig.add_subplot(gs[1, 2])
+
+    tests = ["Reading", "Maths"]
+    for test in tests:
+        ax = axs[test]
+
+        selected = scores.loc[scores["Test"] == test]
+
+        # If there are no results for the student,  output axes with a No Results mesage
+        if len(selected) == 0:
+            for side, spine in ax.spines.items():
+                spine.set_color('dimgrey')
+                spine.set_visible(False)
+            ax.set_title(f"PAT {test}", fontsize=16)
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.text(0.5, 0.5, "No Results", ha="center")
+            continue
+
+        # plot the scores
+        ax.errorbar(selected["Year level number"],
+                    selected["Scale"],
+                    yerr=selected["Error"],
+                    linewidth=0,
+                    elinewidth=1,
+                    color="dimgrey")
+        ax.plot(selected["Year level number"],
+                selected["Scale"],
+                marker=".",
+                ms=12,
+                linewidth=0,
+                color="dimgrey",
+                zorder=3)
+        # shade the background to indicate average/above/below/etc scores
+        for category, data in background_coords[test].items():
+            ax.fill_between(background_coords['xs'],
+                            data['bottom'],
+                            data['top'],
+                            color=data['colour'],
+                            zorder=1)
+
+        # format the graph
+        ax.set_xlim((6, 10))
+        y_min = min(
+            selected["Scale"].min(),
+            background_coords[test]['Well below expected level']['top'][6]) - 5
+        y_max = max(
+            selected["Scale"].max(), background_coords[test]
+            ['Well above expected level']['bottom'][6]) + 5
+        ax.set_ylim((y_min, y_max))
+        ax.tick_params(axis="x", which='both', length=0)
+        ax.set_xticks([i for i in range(6, 11)])
+        ax.set_xticklabels([])
+        ax.set_xticks([i + 0.5 for i in range(6, 10)], minor=True)
+        ax.set_xticklabels([str(i) for i in range(7, 11)], minor=True)
+        ax.set_ylabel("PAT Score", fontsize=12)
+        ax.set_xlabel("Year level", fontsize=12)
+        ax.grid(axis="x", zorder=0)
+        for side, spine in ax.spines.items():
+            spine.set_color('dimgrey')
+            spine.set_visible(True)
+        ax.set_title(f"PAT {test}", fontsize=16)
+
+    # match y axes limits for both graphs
+    y_min = min(axs['Reading'].get_ylim()[0], axs['Maths'].get_ylim()[0])
+    y_max = min(axs['Reading'].get_ylim()[1], axs['Maths'].get_ylim()[1])
+    axs['Reading'].set_ylim((y_min, y_max))
+    axs['Maths'].set_ylim((y_min, y_max))
+
+    # create key
+    cbar.fill_between([0, 1], [0, 0], [5, 5],
+                      color=background_coords['Reading']
+                      ['Well below expected level']['colour'])
+    cbar.fill_between(
+        [0, 1], [5, 5], [25, 25],
+        color=background_coords['Reading']['Below expected level']['colour'])
+    cbar.fill_between(
+        [0, 1], [25, 25], [75, 75],
+        color=background_coords['Reading']['At expected level']['colour'])
+    cbar.fill_between(
+        [0, 1], [75, 75], [95, 95],
+        color=background_coords['Reading']['Above expected level']['colour'])
+    cbar.fill_between([0, 1], [95, 95], [100, 100],
+                      color=background_coords['Reading']
+                      ['Well above expected level']['colour'])
+    cbar.set_xticks([])
+    cbar.set_yticks([])
+    cbar.set_ylim((0, 100))
+    cbar.set_title("Key", pad=15, fontsize=12)
+    cbar.text(1.1, 50, "At expected level", va="center")
+    cbar.text(1.1, 15, "Below expected level", va="center")
+    cbar.text(1.1, 2.5, "Well below expected level", va="center")
+    cbar.text(1.1, 85, "Above expected level", va="center")
+    cbar.text(1.1, 97.5, "Well above expected level", va="center")
+    for side, spine in cbar.spines.items():
+        spine.set_visible(False)
+
+    return fig
+
+
+def student_time_series_plots(
+    pat_scores_csv: str,
+    student_details_csv: str,
+    scale_constructs_json: str,
+    save_dir=".",
+    cmap: mpl.colors.Colormap = mpl.colormaps['PiYG'],
+):
+    """Creates a time series plot of the pat results of all students.
+    
+    Args:
+        pat_scores_csv: The path to a csv containing a pat score summary, as
+            exported from PAT_Sittings.
+        student_details_csv: The path to a csv containing student details, as
+            exported from CompassSession.
+        scale_constructs_json: The path to a saved scale_constructs_json, as
+            exported from OARS.
+        save_dir: The path to save the images of each plot. Will save using a
+            directory structure of save_dir/<year level>/<form group>/ with each
+            image saved as "<student code> <student name> pat timeseries.png"
+        cmap: A diverging colormap to use for indicating results are above or
+            below average.
+    """
+    background_coords = time_series_background_coords(scale_constructs_json,
+                                                      cmap=cmap)
+
+    scores = pd.read_csv(pat_scores_csv)
+    scores.rename(columns={"SUSSI ID": "StudentCode"}, inplace=True)
+    scores["Completed"] = pd.to_datetime(scores["Completed"])
+    scores["Year level number"] = (
+        scores["Year level (at time of test)"].astype(float) - 1 +
+        scores["Completed"].apply(lambda x: x.timetuple().tm_yday) / 365)
+
+    students = pd.read_csv(student_details_csv)
+    students = students.loc[students["Status"] == "Active"].copy()
+    students.rename(columns={"SUSSI ID": "StudentCode"}, inplace=True)
+    students["Full Name"] = students["Preferred Name"] + " " + students[
+        "Last Name"].str.title()
+    students.set_index("StudentCode", inplace=True)
+
+    student_codes = pd.concat([scores["StudentCode"], students.index]).unique()
+    for student_code in student_codes:
+        selected_scores = scores.loc[scores["StudentCode"] == student_code]
+
+        fig = student_time_series_plot(selected_scores, student_code,
+                                       background_coords)
+
+        student = students.loc[student_code]
+        if len(selected_scores) > 0:
+            path = os.path.join(
+                save_dir, f"{student['Year Level']}/{student['Form Group']}")
+            if not os.path.exists(path):
+                os.makedirs(path)
+            fig.savefig(
+                f"{path}/{student_code} {student['Full Name']} PAT timeseries.png",
+                facecolor='w',
+                dpi=150,
+                bbox_inches='tight')
+            plt.close(fig)
